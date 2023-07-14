7e0c2ea91441645516ca85be3ddae1ce6a48cfa0
Freimanis <nik.freymanis@gmail.com>
2023-07-15 03:57:27 +0700

    fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,7 +4,7 @@ import achievement4 from "./achievements/4.png";
 import achievement1 from "./achievements/1.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import logo from "/logo.svg"
+import logo from "./logo.svg"
 import microsoft from "./roadmap/microsoft.svg"
 
 export {
